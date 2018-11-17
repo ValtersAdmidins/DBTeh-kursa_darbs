db673d839b591dfe08e55d174b778e1a577c76b8
ValtersAdmidins <admidins13@gmail.com>
2018-11-17 12:55:50 +0200

    initial commit

---
+++ header.php
@@ -0,0 +1,19 @@
+<!--    KURSA DARBS DBTeh_2018    -->
+<!--    VALTERS ĀDMĪDIŅŠ 3ITB     -->
+<!--        17.11.2018            -->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>DBTeh-uber</title>
+    <meta name="Valters Ādmīdiņš" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+</head>
+<body>
+
+<header>
+
+    test efeef
+
+</header>
